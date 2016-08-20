86e2d4caf339c33776c2330c548a02d1b3985028
jinjames <jamesj.jin@mail.utoronto.ca>
2016-08-20 18:32:08 -0400

    Initial start-up screen

--- app/src/main/java/com/android/connal/planit/MainActivity.java
+++ app/src/main/java/com/android/connal/planit/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,5 +40,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void emailButtonClicked(View view) {
+        Intent intent = new Intent(this, radiusSelectActivity.class);
+        startActivity(intent);
     }
 }
